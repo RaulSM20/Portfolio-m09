48d06b6408736f15cd6e47d66ce9146bf8f4c518
Rsandez <raulsandezmartinez@gmail.com>
2024-05-07 23:55:11 +0200

    up

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+// Leaflet
+
+let map = L.map('map').setView([41.54329, 2.10942], 14);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
